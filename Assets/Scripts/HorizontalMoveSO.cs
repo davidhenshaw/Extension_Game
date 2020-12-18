3f62ca60bf8f262c03d84d67a401b5505b80168c
David Henshaw <davidhenshaw2014@gmail.com>
2020-12-18 08:07:05 -0600

    Set Up State Machine basics on Player
    Only involves creating scriptable objects and moving them around

---
+++ Assets/Scripts/HorizontalMoveSO.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UOP1.StateMachine;
+using UOP1.StateMachine.ScriptableObjects;
+
+[CreateAssetMenu(fileName = "HorizontalMove", menuName = "State Machines/Actions/Horizontal Move")]
+public class HorizontalMoveSO : StateActionSO
+{
+	protected override StateAction CreateAction() => new HorizontalMove();
+}
+
+public class HorizontalMove : StateAction
+{
+	public override void Awake(StateMachine stateMachine)
+	{
+	}
+		
+	public override void OnUpdate()
+	{
+	}
+	
+	// public override void OnStateEnter()
+	// {
+	// }
+	
+	// public override void OnStateExit()
+	// {
+	// }
+}
---
+++ Assets/Scripts/IsGroundedSO.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UOP1.StateMachine;
+using UOP1.StateMachine.ScriptableObjects;
+
+[CreateAssetMenu(fileName = "IsGrounded", menuName = "State Machines/Conditions/Is Grounded")]
+public class IsGroundedSO : StateConditionSO
+{
+	protected override Condition CreateCondition() => new IsGrounded();
+}
+
+public class IsGrounded : Condition
+{
+	public override void Awake(StateMachine stateMachine)
+	{
+	}
+		
+	protected override bool Statement()
+	{
+		return true;
+	}
+	
+	// public override void OnStateEnter()
+	// {
+	// }
+	
+	// public override void OnStateExit()
+	// {
+	// }
+}
