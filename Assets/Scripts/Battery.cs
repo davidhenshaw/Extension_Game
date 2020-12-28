4cb436ad1949e4245ad627d08df2b4eba6a6a26c
David Henshaw <davidhenshaw2014@gmail.com>
2020-12-28 14:01:40 -0600

    Implement power source framework
    Also makes Cord a child of the player's Battery

---
+++ Assets/Scripts/Battery.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Battery : MonoBehaviour, IPowerSink
+{
+    [SerializeField] float _chargeRate;
+    [SerializeField] float _capacity;
+    float _currCharge;
+
+    IPowerSource _source;
+
+    public void OnConnect(IPowerSource source)
+    {
+        _source = source;
+    }
+
+    public void OnDisconnect(IPowerSource source)
+    {
+        _source = null;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _currCharge = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(_source != null)
+        {
+            _currCharge += _source.GetCharge(_chargeRate * Time.deltaTime);
+            _currCharge = Mathf.Clamp(_currCharge, 0, _capacity);
+
+            Debug.Log("Battery charge: " + _currCharge);
+        }
+    }
+
+}
---
+++ Assets/Scripts/Conductor.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conductor : MonoBehaviour
+{
+    IPowerSource _source;
+    IPowerSink _sink;
+
+    private void Start()
+    {
+        _sink = GetComponentInParent<IPowerSink>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        _source = collision.GetComponentInChildren<IPowerSource>();
+
+        if(_source != null)
+        {
+            _source.OnConnect(_sink);
+            _sink.OnConnect(_source);
+        }
+    }
+}
+
+public interface IPowerSink
+{
+    void OnConnect(IPowerSource source);
+    void OnDisconnect(IPowerSource source);
+}
+
+public interface IPowerSource
+{
+    float GetCharge(float requested);
+    void OnConnect(IPowerSink sink);
+    void OnDisconnect(IPowerSink sink);
+}
