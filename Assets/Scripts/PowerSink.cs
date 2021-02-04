163eb906377796d854c0cfff42c6a9e1f4e44ecc
David Henshaw <davidhenshaw2014@gmail.com>
2021-02-04 11:27:10 -0600

    Refactor Player power configuration

--- Assets/Scripts/PowerSink.cs
+++ Assets/Scripts/PowerSink.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class PowerListener : MonoBehaviour, IPowerSink
+public class PowerSink : MonoBehaviour, IPowerSink
 {
     [SerializeField] float powerDraw = 10f;
 
