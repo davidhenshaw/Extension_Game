23d9094579d8695f2d4935d62c178d0d5a7394b7
David Henshaw <davidhenshaw2014@gmail.com>
2021-02-04 10:49:16 -0600

    Remove legacy code

--- Assets/Scripts/Player Controls/PlayerController.cs
+++ Assets/Scripts/Player Controls/PlayerController.cs
@@ -12,78 +12,20 @@ public class PlayerController : MonoBehaviour
     Vector2 aimDir;
     DistanceJoint2D joint;
 
-    private void Awake()
-    {
-    }
-
     // Start is called before the first frame update
     void Start()
     {
         aimDir = new Vector2(1, 1);
-        //_cord.Plug.pluggedIn += CreateJoint;
-        //_cord.Plug.disconnected += DestroyJoint;
     }
 
     // Update is called once per frame
     void Update()
     {
-
         ReadInput();
     }
 
-    private void OnEnable()
-    {
-        //if (_cord.Plug == null)
-        //    return;
-
-        //_cord.Plug.pluggedIn += CreateJoint;
-        //_cord.Plug.disconnected += DestroyJoint;
-    }
-
-    private void OnDisable()
-    {
-        //_cord.Plug.pluggedIn -= CreateJoint;
-        //_cord.Plug.disconnected -= DestroyJoint;
-    }
-
-    //void CreateJoint(Rigidbody2D rb)
-    //{
-    //    if (joint != null)
-    //        return;
-
-    //    joint = gameObject.AddComponent<DistanceJoint2D>();
-    //    joint.autoConfigureDistance = false;
-    //    joint.connectedBody = rb;
-
-    //    joint.autoConfigureDistance = false;
-    //    joint.distance = _cordLength;
-    //    joint.maxDistanceOnly = true;
-    //}
-
-    //void DestroyJoint(Rigidbody2D rb)
-    //{
-    //    if (joint == null)
-    //        return;
-
-    //    joint = gameObject.GetComponent<DistanceJoint2D>();
-    //    Destroy(joint);
-    //}
-
     void ReadInput()
     {
-        //if(Input.GetKeyDown(KeyCode.R))
-        //{
-        //    if (_cord.IsRetracted)
-        //        _cord.Release();
-        //    else
-        //        _cord.Retract();
-        //}
-
-        //if (Input.GetKeyDown(KeyCode.E))
-        //{
-        //    _cord.EjectPlug(aimDir);
-        //}
-
         if(Input.GetMouseButtonDown(0))//Left click
         {
             if(_ability1 != null)
